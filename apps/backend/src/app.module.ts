8037e1fddaf8217f9192057b7e53f7f57c5e6bd6
vyasworks <67800715+vyasworks@users.noreply.github.com>
2022-10-03 11:46:41 -0700

    init sales module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppLogger } from './common/logger.service';
+import { SalesModule } from './sales/sales.module';
 
 @Module({
-  imports: [],
+  imports: [SalesModule],
   controllers: [AppController],
   providers: [AppService, AppLogger],
 })
