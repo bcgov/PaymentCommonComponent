e8f87c8b1ca8741a8699e5201d048f44dbcaa606
Vyas <67800715+vyasworks@users.noreply.github.com>
2023-01-06 13:09:26 -0800

    Setup SFTP servers and users

---
+++ terraform/secrets.tf
@@ -0,0 +1,9 @@
+# These should be manually populated in the console for each environment
+
+data "aws_ssm_parameter" "sftp_user_sbc" {
+  name = "/${var.project_code}/${var.target_env}/sftp/user/sbc"
+}
+
+data "aws_ssm_parameter" "sftp_user_pcc" {
+  name = "/${var.project_code}/${var.target_env}/sftp/user/pcc"
+}
