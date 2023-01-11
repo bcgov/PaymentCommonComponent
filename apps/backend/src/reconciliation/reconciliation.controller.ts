9c02b564543442937293c641a9c6cd631486c75e
chelsea-EYDS <chelsea.brown1@ca.ey.com>
2023-01-10 16:46:15 -0800

    created endpoint for parsing and testing

--- apps/backend/src/reconciliation/reconciliation.controller.ts
+++ apps/backend/src/reconciliation/reconciliation.controller.ts
@@ -6,12 +6,22 @@ import {
   Controller,
   ClassSerializerInterceptor,
   Inject,
-  Logger
+  Logger,
+  HttpCode,
+  HttpStatus
 } from '@nestjs/common';
-import { ApiOperation, ApiConsumes, ApiBody, ApiTags } from '@nestjs/swagger';
+import {
+  ApiResponse,
+  ApiOperation,
+  ApiConsumes,
+  ApiBody,
+  ApiTags
+} from '@nestjs/swagger';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { AppLogger } from '../common/logger.service';
 import { ReconciliationService } from './reconciliation.service';
+import { GarmsDTO } from './dto/garms.dto';
+import { TransactionEntity } from './entities/transaction.entity';
 
 @Controller('parse')
 @ApiTags('Flat File and Garms Json Test Parser API')
@@ -60,25 +70,62 @@ export class ReconciliationController {
     );
   }
 
+  @HttpCode(HttpStatus.CREATED)
   @ApiOperation({
     summary: 'Post Transaction Data'
   })
-  @Post('garms-json')
-  @UseInterceptors(ClassSerializerInterceptor)
-  @ApiConsumes('multipart/form-data')
+  @ApiConsumes('application/json')
   @ApiBody({
     schema: {
-      type: 'object',
-      properties: {
-        file: {
-          type: 'string',
-          format: 'binary'
+      example: [
+        {
+          sales_transaction_id: '20221212-00002-1000001',
+          sales_transaction_date: '2022-12-12-11.57.00.986053',
+          fiscal_close_date: '20221212',
+          payment_total: 52.5,
+          void_indicator: ' ',
+          transaction_reference: '',
+          payments: [
+            {
+              amount: 52.5,
+              currency: 'CAD',
+              exchange_rate: '0.000',
+              method: '01'
+            }
+          ],
+          source: { location_id: '00001' }
         }
+      ]
+    }
+  })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    schema: {
+      example: {
+        data: [
+          {
+            transaction_id: '20221212-00002-1000004',
+            location_id: 1,
+            transaction_date: '2022-12-12',
+            transaction_time: '11.57.00.9',
+            payment_total: 52.5,
+            payments: [
+              {
+                method: '01',
+                amount: 52.5,
+                currency: 'CAD',
+                exchange_rate: '0.000'
+              }
+            ]
+          }
+        ]
       }
     }
   })
-  @UseInterceptors(FileInterceptor('file'))
-  uploadGarmsJsonFile(@UploadedFile() file: Express.Multer.File) {
-    return this.reconService.readAndParseGarms(file.originalname, file.buffer);
+  @Post('garms-json')
+  async postTransactionData(
+    @Body() garmsDTO: GarmsDTO[]
+  ): Promise<TransactionEntity[]> {
+    return await this.reconService.parseAndReturnGarms(garmsDTO);
   }
 }
