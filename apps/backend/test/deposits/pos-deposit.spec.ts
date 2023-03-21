6406164d3e323f6bcd226f2816aed4b40bc0efd5
chelsea-EYDS <chelsea.brown1@ca.ey.com>
2023-03-21 12:35:03 -0700

    [CCFPCM-395] created basic services for tests

--- apps/backend/test/deposits/pos-deposit.spec.ts
+++ apps/backend/test/deposits/pos-deposit.spec.ts
@@ -2,7 +2,9 @@ import { Logger } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { POSDepositEntity } from './../../src/deposits/entities/pos-deposit.entity';
+import { LocationEntity } from './../../src/location/entities/master-location-data.entity';
 import { PosDepositService } from '../../src/deposits/pos-deposit.service';
+import { LocationService } from '../../src/location/location.service';
 
 describe('POSDepositService', () => {
   let service: PosDepositService;
@@ -14,9 +16,14 @@ describe('POSDepositService', () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         PosDepositService,
+        LocationService,
         Logger,
         {
           provide: getRepositoryToken(POSDepositEntity),
+          useValue: mockedRepo
+        },
+        {
+          provide: getRepositoryToken(LocationEntity),
           useValue: mockedRepo
         }
       ]
