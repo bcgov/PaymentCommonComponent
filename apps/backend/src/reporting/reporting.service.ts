b7c02a22a3481ed667e76db119ce3fa1ab5a990b
chelsea-EYDS <chelsea.brown1@ca.ey.com>
2023-03-25 11:02:56 -0700

    [CCFPCM380] third report

--- apps/backend/src/reporting/reporting.service.ts
+++ apps/backend/src/reporting/reporting.service.ts
@@ -187,13 +187,15 @@ export class ReportingService {
         settlement_date: format(new Date(itm.deposit_date), 'yyyy-MM-dd'),
         amount: itm.deposit_amt_cdn
       })),
-      ...posDeposits.map((itm) => ({
-        ...itm,
-        ...casLocationData,
-        settlement_date: format(new Date(itm.settlement_date), 'yyyy-MM-dd'),
-        card_vendor: itm.card_vendor,
-        amount: parseFloat(itm.transaction_amt.toString())
-      }))
+      ...posDeposits
+        .filter((itm) => itm.transaction_amt.toString() !== '0.00')
+        .map((itm) => ({
+          ...itm,
+          ...casLocationData,
+          settlement_date: format(new Date(itm.settlement_date), 'yyyy-MM-dd'),
+          card_vendor: itm.card_vendor,
+          amount: parseFloat(itm.transaction_amt.toString())
+        }))
     ]);
 
     return report;
