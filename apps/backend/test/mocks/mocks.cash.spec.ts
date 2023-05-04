8ef5b4029afc6fc7d4e07cd0df6cf78cea3cf73d
chelsea-EYDS <chelsea.brown1@ca.ey.com>
2023-05-04 10:27:07 -0700

    [CCFPCM-422] Cash Recon Unit Tests

--- apps/backend/test/mocks/mocks.cash.spec.ts
+++ apps/backend/test/mocks/mocks.cash.spec.ts
@@ -1,3 +1,4 @@
+import { compareAsc } from 'date-fns';
 import { CashDepositMock } from './classes/cash_deposit_mock';
 import { PaymentMock } from './classes/payment_mock';
 import { MockData } from './mocks';
@@ -29,6 +30,27 @@ describe('Tests the generated mock data', () => {
     };
     expect(expected.payments.map((itm) => itm.amount)).toEqual(
       expected.deposits.map((itm) => itm.amount)
+    );
+  });
+
+  it('should generate cash deposits with a date greater than the date of the last payment', () => {
+    const expected = {
+      deposits: cashDepositsMock.map((itm) => ({
+        date: itm.deposit_date,
+        amount: itm.deposit_amt_cdn
+      })),
+      payments: aggregatedPayments(cashPaymentsMock).map((itm) => ({
+        date: itm.fiscal_close_date,
+        amount: itm.amount
+      }))
+    };
+    expected.deposits.forEach((deposit) =>
+      expected.payments.forEach((payment) =>
+        expect(
+          // Compare the two dates and return 1 if the first date is after the second, -1 if the first date is before the second or 0 if dates are equal.
+          compareAsc(new Date(deposit.date), new Date(payment.date))
+        ).toEqual(1)
+      )
     );
   });
 });
