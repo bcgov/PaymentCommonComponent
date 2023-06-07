043dae8855a3c024d9a0b02c9adb0ac1376ac340
chelsea-EYDS <chelsea.brown1@ca.ey.com>
2023-06-06 19:24:19 -0700

    [CCFPCM-526] round 3 bug

--- apps/backend/src/reconciliation/pos-reconciliation.service.ts
+++ apps/backend/src/reconciliation/pos-reconciliation.service.ts
@@ -241,13 +241,16 @@ export class PosReconciliationService {
           depositsWithAmount[`${dateToFind}-${paymentMethod}`];
         if (!deposits?.length) {
           // For round three, a deposit can be one business day apart from the payment
+          // Could be the day before, or the day after, so we search those days for the amount
           if (posHeuristicRound === PosHeuristicRound.THREE) {
-            const dayBeforeDateToFind = subtractBusinessDaysNoTimezone(
-              dateToFind,
-              1
-            );
-            deposits =
-              depositsWithAmount[`${dayBeforeDateToFind}-${paymentMethod}`];
+            if (!deposits?.length) {
+              const dayBeforeDateToFind = subtractBusinessDaysNoTimezone(
+                dateToFind,
+                1
+              );
+              deposits =
+                depositsWithAmount[`${dayBeforeDateToFind}-${paymentMethod}`];
+            }
           }
         }
         if (deposits?.length) {
