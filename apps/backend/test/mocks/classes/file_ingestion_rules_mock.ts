fb6ea1f2ed2535fff7dbe6c9f2176dbf9a3f5a57
Noel Feliciano <noel.feliciano@ca.ey.com>
2023-06-14 00:43:11 -0700

    tests: Add tests for parse service

---
+++ apps/backend/test/mocks/classes/file_ingestion_rules_mock.ts
@@ -0,0 +1,19 @@
+import { faker } from '@faker-js/faker';
+import { FileIngestionRulesEntity } from '../../../src/parse/entities/file-ingestion-rules.entity';
+
+export class FileIngestionRulesMock extends FileIngestionRulesEntity {
+  constructor(
+    program: string,
+    cashChequesFilename?: string | undefined,
+    posFilename?: string | undefined,
+    transactionsFilename?: string | undefined
+  ) {
+    super();
+    this.id = faker.datatype.uuid();
+    this.program = program;
+    this.retries = faker.datatype.number({ min: 0, max: 3 });
+    this.cashChequesFilename = cashChequesFilename;
+    this.posFilename = posFilename;
+    this.transactionsFilename = transactionsFilename;
+  }
+}
