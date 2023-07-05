55553d523533074f5c8d8134693aa1516c501302
chelsea-EYDS <chelsea.brown1@ca.ey.com>
2023-07-05 08:43:15 -0700

    [CCFPCM-535] Labour migrations

--- apps/backend/src/reporting/cas-report/cas-report.ts
+++ apps/backend/src/reporting/cas-report/cas-report.ts
@@ -7,7 +7,7 @@ export class CasReport {
   amount: number;
   location_id: number;
   loction_name: string;
-  dist_client_code: number;
+  dist_client_code: string;
   dist_resp_code: string;
   dist_stob_code: number;
   dist_service_line_code: number;
@@ -24,7 +24,7 @@ export class CasReport {
     this.amount = new Decimal(amount).toDecimalPlaces(2).toNumber();
     this.location_id = location.location_id;
     this.loction_name = location.description;
-    this.dist_client_code = location.ministry_client;
+    this.dist_client_code = location.ministry_client.toString();
     this.dist_resp_code = location.resp_code;
     this.dist_stob_code = location.stob_code;
     this.dist_service_line_code = location.service_line_code;
