4d78173848cdeccb328b9ae439fbc18f9fa9d885
Chelsea Brown <chelsea.brown1@ca.ey.com>
2023-07-27 12:05:58 -0700

    [CCFPCM-0574]

--- apps/backend/src/lambdas/interface.ts
+++ apps/backend/src/lambdas/interface.ts
@@ -1,29 +1,4 @@
-import { Ministries } from 'src/constants';
-
-export interface ParseRecord {
-  s3: {
-    s3SchemaVersion: string;
-    configurationId: string;
-    bucket: {
-      name: string;
-      ownerIdentity: {
-        principalId: string;
-      };
-      arn: string;
-    };
-    object: {
-      key: string;
-      size: string;
-      eTag: string;
-      versionId: string;
-      sequencer: string;
-    };
-  };
-}
-
-export interface ParseEvent {
-  Records: ParseRecord[];
-}
+import { Ministries } from '../constants';
 
 export interface ReconciliationMessage {
   program: Ministries;
