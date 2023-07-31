2d958f061aa3c3fd555f7c2107ff6af5cbfe4cfb
Chelsea Brown <chelsea.brown1@ca.ey.com>
2023-07-31 12:39:06 -0700

    [CCFPCM-0340] migrations update

--- apps/backend/src/database/migrations/1690503718109-daily-date.ts
+++ apps/backend/src/database/migrations/1690503718109-daily-date.ts
@@ -5,29 +5,17 @@ export class migration1690503718109 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "pos_deposit" ADD "file_created_date" TIMESTAMP`
+      `ALTER TABLE "transaction" ADD "file_created_date" date`
     );
     await queryRunner.query(
-      `ALTER TABLE "cash_deposit" ADD "file_created_date" TIMESTAMP`
+      `ALTER TABLE "pos_deposit" ADD "file_created_date" date`
     );
     await queryRunner.query(
-      `ALTER TABLE "transaction" ADD "file_created_date" TIMESTAMP`
-    );
-    await queryRunner.query(
-      `ALTER TABLE "pos_deposit" ALTER COLUMN "file_created_date" TYPE date USING "file_created_date"::date`
-    );
-    await queryRunner.query(
-      `ALTER TABLE "cash_deposit" ALTER COLUMN "file_created_date" TYPE date USING "file_created_date"::date`
-    );
-    await queryRunner.query(
-      `ALTER TABLE "transaction" ALTER COLUMN "file_created_date" TYPE date USING "file_created_date"::date`
+      `ALTER TABLE "cash_deposit" ADD "file_created_date" date`
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE "file_uploaded" DROP COLUMN "file_created_date"`
-    );
     await queryRunner.query(
       `ALTER TABLE "cash_deposit" DROP COLUMN "file_created_date"`
     );
@@ -35,13 +23,7 @@ export class migration1690503718109 implements MigrationInterface {
       `ALTER TABLE "pos_deposit" DROP COLUMN "file_created_date"`
     );
     await queryRunner.query(
-      `ALTER TABLE "pos_deposit" ALTER COLUMN "file_created_date" TYPE TIMESTAMP USING "file_created_date"::timestamp`
-    );
-    await queryRunner.query(
-      `ALTER TABLE "cash_deposit" ALTER COLUMN "file_created_date" TYPE TIMESTAMP USING "file_created_date"::timestamp`
-    );
-    await queryRunner.query(
-      `ALTER TABLE "transaction" ALTER COLUMN "file_created_date" TYPE TIMESTAMP USING "file_created_date"::timestamp`
+      `ALTER TABLE "transaction" DROP COLUMN "file_created_date"`
     );
   }
 }
