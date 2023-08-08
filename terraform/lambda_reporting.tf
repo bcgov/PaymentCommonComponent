da8d9f8b0d3c517889bee5c64b645ece4db91c2a
Chelsea Brown <chelsea.brown1@ca.ey.com>
2023-08-08 13:07:41 -0700

    [FIX] fix SNS topics to work in each env

--- terraform/lambda_reporting.tf
+++ terraform/lambda_reporting.tf
@@ -26,9 +26,9 @@ resource "aws_lambda_function" "reports" {
       MAIL_SERVICE_KEY              = data.aws_ssm_parameter.gcnotify_key.value
       MAIL_SERVICE_BASE_URL         = var.mail_base_url
       MAIL_SERVICE_DEFAULT_TO_EMAIL = var.mail_default_to
-      SNS_PARSER_RESULTS_TOPIC      = var.sns_parser_topic
-      SNS_RECONCILER_RESULTS_TOPIC  = var.sns_reconciler_topic
-      SNS_BATCH_RECONCILE_TOPIC     = var.sns_batch_reconcile_topic
+      SNS_PARSER_RESULTS_TOPIC      = "arn:aws:sns:${var.region}:${var.target_aws_account_id}:parser-results-topic"
+      SNS_RECONCILER_RESULTS_TOPIC  = "arn:aws:sns:${var.region}:${var.target_aws_account_id}:reconciliation-results-topic"
+      SNS_BATCH_RECONCILE_TOPIC     = "arn:aws:sns:${var.region}:${var.target_aws_account_id}:batch-reconcile-topic"
       DISABLE_AUTOMATED_RECONCILIATION = var.disable_automated_reconciliation
     }
   }
