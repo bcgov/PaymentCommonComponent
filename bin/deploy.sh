98910ebdcc1929b077a4de959980dde306b36df6
Simen Fivelstad Smaaberg <simen@freshworks.io>
2023-08-15 14:42:19 -0700

    Updated spacing of deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,21 +5,21 @@
 # Usage: aws-publish-layer
 #####
 function aws-publish-layer() {
-	aws lambda publish-layer-version \
-		--layer-name "ppc-dependencies" \
-		--content S3Bucket=$APP_SRC_BUCKET,S3Key=$COMMIT_SHA/layer/nodejs.zip \
-		--compatible-runtimes nodejs18.x \
-		--region ca-central-1 \
-		--output text \
-		--query 'LayerVersionArn' \
-		--no-cli-pager
+  aws lambda publish-layer-version \
+    --layer-name "ppc-dependencies" \
+    --content S3Bucket=$APP_SRC_BUCKET,S3Key=$COMMIT_SHA/layer/nodejs.zip \
+    --compatible-runtimes nodejs18.x \
+    --region ca-central-1 \
+    --output text \
+    --query 'LayerVersionArn' \
+    --no-cli-pager
 }
 
 function aws-get-latest-layer-version() {
-    aws lambda list-layer-versions \
-            --layer-name ppc-dependencies \
-            --region ca-central-1 \
-            --query 'LayerVersions[0].LayerVersionArn'
+  aws lambda list-layer-versions \
+    --layer-name ppc-dependencies \
+    --region ca-central-1 \
+    --query 'LayerVersions[0].LayerVersionArn'
 }
 
 #####
@@ -27,23 +27,23 @@ function aws-get-latest-layer-version() {
 # Usage: aws-update-function-code <function-name>
 #####
 function aws-update-function-code() {
-	echo "Updating function code for $1...\n"
-	aws lambda update-function-code \
-        --function-name $1 \
-        --s3-bucket=$APP_SRC_BUCKET \
-        --s3-key=$COMMIT_SHA/backend/backend.zip \
-        --region ca-central-1 \
-        --no-cli-pager
+  echo "Updating function code for $1...\n"
+  aws lambda update-function-code \
+    --function-name $1 \
+    --s3-bucket=$APP_SRC_BUCKET \
+    --s3-key=$COMMIT_SHA/backend/backend.zip \
+    --region ca-central-1 \
+    --no-cli-pager > /dev/null
 
-    latest_layer_version="$(aws-get-latest-layer-version)"
+  latest_layer_version="$(aws-get-latest-layer-version)"
 
-	echo "Updating function layer for $1 to layer version $latest_layer_version \n"
+  echo "Updating function layer for $1 to layer version $latest_layer_version"
 
-	aws lambda update-function-configuration \
-        --function-name $1 \
-        --layers [$latest_layer_version] \
-        --region ca-central-1 \
-        --no-cli-pager
+  aws lambda update-function-configuration \
+    --function-name $1 \
+    --layers [$latest_layer_version] \
+    --region ca-central-1 \
+    --no-cli-pager > /dev/null
 }
 
 #####
@@ -51,17 +51,14 @@ function aws-update-function-code() {
 # Usage: aws-deploy-function <function-name> <function-name> ... (as many as you want)
 #####
 function aws-deploy-function() {
-    # echo "$@"
-    echo "$#"
-    for funct in "${@:2}"; do
-        echo $funct;
-        aws-update-function-code $funct
-    done
+  for funct in "${@:2}"; do
+    echo $funct
+    aws-update-function-code $funct
+  done
 }
 
 # Check if the function exists and execute the given command
-if declare -f "$1" > /dev/null
-then
+if declare -f "$1" >/dev/null; then
   "$@" "${@:2}"
 else
   echo "'$1' is not a known function name" >&2
