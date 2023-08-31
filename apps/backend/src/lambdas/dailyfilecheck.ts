c18988f82a4ca06f0459b9079ee3b5dc37744c2d
Noel Feliciano <noel.feliciano@ca.ey.com>
2023-08-31 09:19:27 -0700

    fix: alert for all unsuccessful statuses in past 7 days

--- apps/backend/src/lambdas/dailyfilecheck.ts
+++ apps/backend/src/lambdas/dailyfilecheck.ts
@@ -4,14 +4,48 @@ import { format, isSameDay, subDays } from 'date-fns';
 import { ProgramTemplateName } from './const';
 import { AppModule } from '../app.module';
 import { AppLogger } from '../logger/logger.service';
-import { AlertDestinationEntity } from '../notification/entities/alert-destination.entity';
 import { MAIL_TEMPLATE_ENUM } from '../notification/mail-templates';
 import { MailService } from '../notification/mail.service';
 import { NotificationService } from '../notification/notification.service';
+import { ProgramRequiredFileEntity } from '../parse/entities/program-required-file.entity';
 
 const ALERT_EXPIRY_DAYS = 7; // Num days before we stop alerting for a LOB if they haven't submitted all files for a day
 
 export const handler = async (event: unknown, context?: Context) => {
+  const sendMissingFilesEmail = async (
+    program: string,
+    destinations: string[],
+    errorsMessage: string
+  ) => {
+    appLogger.log('Daily file check showing errors:');
+    appLogger.log(errorsMessage);
+
+    appLogger.log('\n\n=========Alerts Sent for Daily Upload: =========\n');
+    appLogger.error(`Sending an alert to prompt ${program} to complete upload`);
+
+    await mailService.sendEmailAlertBulk(
+      MAIL_TEMPLATE_ENUM.FILES_MISSING_ALERT,
+      destinations,
+      [
+        {
+          fieldName: 'date',
+          content: format(new Date(), 'MMM do, yyyy'),
+        },
+        {
+          fieldName: 'ministryDivision',
+          content: `${
+            process.env.NODE_ENV !== 'production' &&
+            `[${process.env.NODE_ENV}] `
+          }${program}`,
+        },
+        {
+          fieldName: 'error',
+          content: errorsMessage,
+        },
+      ]
+    );
+  };
+
   const app = await NestFactory.createApplicationContext(AppModule);
   const appLogger = app.get(AppLogger);
   appLogger.setContext('Daily File Check Lambda');
@@ -34,6 +68,7 @@ export const handler = async (event: unknown, context?: Context) => {
     allRecentStatuses.filter((status) => isSameDay(status.created_at, date))
       .length === 0
   ) {
+    appLogger.log('No uploads today, we will not send out any alerts');
     // No files have been sent in today, skip alerting for off days
     // Assumption: At least one file upload should have arrived on time from any LOB
     return;
@@ -51,60 +86,62 @@ export const handler = async (event: unknown, context?: Context) => {
       `${unsuccessfulStatuses.length} unsuccessful days in the past ${ALERT_EXPIRY_DAYS} days`
     );
 
-    // Alert - This program has files missing either today or in past days
+    const allMissingFiles: ProgramRequiredFileEntity[] = [];
+    unsuccessfulStatuses.forEach((st) => {
+      const mfiles = notificationService.findMissingDailyFiles(rule, st.files);
+      allMissingFiles.push(...mfiles);
+    });
+
     if (unsuccessfulStatuses.length > 0) {
-      const earliestStatus = unsuccessfulStatuses[0]; // Query should be ordered by daily date
-      const errors: string[] = [];
       const program =
         ProgramTemplateName[rule.program as keyof typeof ProgramTemplateName];
 
-      const incompleteString = `Daily Upload for ${program} is incomplete for date: ${earliestStatus.dailyDate}.\n`;
-      errors.push(incompleteString);
-
-      const missingFiles = notificationService.findMissingDailyFiles(
-        rule,
-        earliestStatus.files
+      const allAlertDestinations = await mailService.getAlertDestinations(
+        program,
+        allMissingFiles.map((mf) => mf.filename)
       );
-      missingFiles.forEach((file) => {
-        errors.push(`Missing a ${file.fileType} - needs ${file.filename}\n`);
-      });
 
-      appLogger.log(errors.join(' '));
-      const alertDestinationEntities: AlertDestinationEntity[] =
-        await mailService.getAlertDestinations(
-          program,
-          missingFiles.map((mf) => mf.filename)
+      // Send a summary to destinations with 'ALL' and 'RULE' of all missing files
+      const ruleDestinations = allAlertDestinations.filter(
+        (ad) => ad.allAlerts || ad.rule?.id === rule.id
+      );
+      const ruleErrors: string[] = [];
+      for (const status of unsuccessfulStatuses) {
+        const incompleteString = `Daily Upload for ${program} is incomplete for date: ${status.dailyDate}.\n`;
+        ruleErrors.push(incompleteString);
+        const missingFiles = notificationService.findMissingDailyFiles(
+          rule,
+          status.files
         );
 
-      const alertDestinations = alertDestinationEntities.map(
-        (itm) => itm.destination
-      );
+        // For each missing file, send an email to destinations that point to the specific filetype
+        const fileErrors: string[] = [];
+        missingFiles.map(async (file) => {
+          const fileAlertDestinations = allAlertDestinations.filter(
+            (ad) => ad.requiredFile?.id === file.id
+          );
+          if (fileAlertDestinations.length > 0) {
+            fileErrors.push(incompleteString);
+            fileErrors.push(
+              `Missing a ${file.fileType} - needs ${file.filename}\n`
+            );
+            await sendMissingFilesEmail(
+              program,
+              fileAlertDestinations.map((ad) => ad.destination),
+              fileErrors.join(' ')
+            );
+          }
 
-      if (!alertDestinations.length || !program) {
-        continue;
+          // Add this missing file string to the more general rule / all error message
+          ruleErrors.push(
+            `Missing a ${file.fileType} - needs ${file.filename}\n`
+          );
+        });
       }
-
-      appLogger.log('\n\n=========Alerts Sent for Daily Upload: =========\n');
-      appLogger.error(
-        `Sending an alert to prompt ${program} to complete upload`
-      );
-      await mailService.sendEmailAlertBulk(
-        MAIL_TEMPLATE_ENUM.FILES_MISSING_ALERT,
-        alertDestinations.map((ad) => ad),
-        [
-          {
-            fieldName: 'date',
-            content: format(new Date(), 'MMM do, yyyy'),
-          },
-          {
-            fieldName: 'ministryDivision',
-            content: program,
-          },
-          {
-            fieldName: 'error',
-            content: errors.join(' '),
-          },
-        ]
+      await sendMissingFilesEmail(
+        program,
+        ruleDestinations.map((ad) => ad.destination),
+        ruleErrors.join(' ')
       );
     }
   }
