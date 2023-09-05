25bf3b210c24ac17b8c3045be2bdd63ab6cd97e1
Simen Fivelstad Smaaberg <66635118+simensma-fresh@users.noreply.github.com>
2023-09-05 09:48:34 -0700

    [CCFPCM-602] Fixed db migrations not running in dev (#228)

--- apps/backend/src/database/datasource.ts
+++ apps/backend/src/database/datasource.ts
@@ -2,6 +2,40 @@ import { DataSource } from 'typeorm';
 import { entities } from './entity.config';
 import { dbLogger, logLevels } from './helpers';
 
+const migrationsFolder = [__dirname + '/migrations/**/*{.ts,.js}'];
+
+// Load migrations using Webpack contexts when building the app using webpack.
+// This is necessary seeing as webpack bundles all app files, so there won't
+// be any migrations in the /migrations folder for typeorm to pick up.
+const loadMigrationsWithWebpack = () => {
+  const context = (<any>require).context('./migrations', true, /\.js|\.ts/); // eslint-disable-line @typescript-eslint/no-explicit-any
+  return context
+    .keys()
+    .sort()
+    .map((fn: string) => {
+      const module = context(fn);
+
+      return Object.keys(module).reduce((result, key) => {
+        const exportedEntity = module[key];
+
+        if (typeof exportedEntity === 'function') {
+          return result.concat(exportedEntity);
+        }
+
+        return result;
+      }, []);
+    })
+    .flat();
+};
+
+let migrations;
+
+try {
+  migrations = loadMigrationsWithWebpack();
+} catch (e) {
+  migrations = migrationsFolder;
+}
+
 export default new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST ?? 'localhost',
@@ -10,7 +44,7 @@ export default new DataSource({
   password: process.env.DB_PASSWORD ?? 'postgres',
   database: process.env.DB_NAME ?? 'pcc',
   entities,
-  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+  migrations: migrations,
   synchronize: false,
   logging: logLevels,
   logger: dbLogger,
