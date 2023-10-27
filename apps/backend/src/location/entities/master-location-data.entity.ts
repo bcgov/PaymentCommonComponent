b6f02d3777113c06d991ad9672552c32cf129a74
Chelsea Brown <chelsea.brown1@ca.ey.com>
2023-10-27 13:15:24 -0700

    migrations: remove PK from location

--- apps/backend/src/location/entities/master-location-data.entity.ts
+++ apps/backend/src/location/entities/master-location-data.entity.ts
@@ -5,12 +5,6 @@ import { BaseLocationEntity } from './location_base';
 @Entity('master_location_data')
 export class MasterLocationEntity extends BaseLocationEntity {
   @Column('varchar', { length: 15, nullable: false })
-  source_id: string;
-
-  @Column({ type: 'int4', nullable: false })
-  location_id: number;
-
-  @Column('varchar', { length: 15, nullable: false })
   method: string;
 
   @Column({ type: 'int4', nullable: true })
