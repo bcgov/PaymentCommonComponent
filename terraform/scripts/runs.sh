fd25ef388079acc600349f8d4435a0da6cdceaef
Chelsea Brown <chelsea.brown1@ca.ey.com>
2023-11-14 14:55:10 -0800

    tf: remove redundant blocks

--- terraform/scripts/runs.sh
+++ terraform/scripts/runs.sh
@@ -3,10 +3,10 @@
 set -e
 
 TOKEN=${1?"Enter TFC Token !"}
-ENV=${2?"Enter ENV !"}
+ENV_NAME=${2?"Enter ENV !"}
 
 curl \
     -H "Authorization: Bearer $TOKEN" \
     -X GET \
-    https://app.terraform.io/api/v2/organizations/bcgov/workspaces/uux0vy-$ENV |
+    https://app.terraform.io/api/v2/organizations/bcgov/workspaces/uux0vy-$ENV_NAME |
     jq -r '.data.relationships."current-run".data.id'
