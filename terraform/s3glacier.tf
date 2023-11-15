fb659620a4b4cab74e8c526f1c5081c35df2c9cf
Noel Feliciano <noel.feliciano@ca.ey.com>
2023-11-15 01:47:24 -0800

    fix: policy resources fix, locals

--- terraform/s3glacier.tf
+++ terraform/s3glacier.tf
@@ -1,3 +1,7 @@
+locals {
+  vault_name = "PccArchive"
+}
+
 data "aws_iam_policy_document" "archive_read_only" {
   statement {
     sid    = "add-read-only-perm"
@@ -13,12 +17,14 @@ data "aws_iam_policy_document" "archive_read_only" {
       "glacier:GetJobOutput",
     ]
 
-    resources = ["arn:aws:s3:::*"]
+    resources = [
+      "arn:aws:glacier:${var.region}:${var.target_aws_account_id}:vaults/${local.vault_name}"
+    ]
   }
 }
 
 resource "aws_glacier_vault" "pcc_archive" {
-  name = "PccArchive"
+  name = local.vault_name
 
   access_policy = data.aws_iam_policy_document.archive_read_only.json
 
